81860663770596668dce8afa40a7dea2a1f3d59c
david-calderon5 <david.calderon5@educa.madrid.org>
2025-02-02 21:36:08 +0100

    Commit 2

--- src/main/java/org/example/SwingChat.java
+++ src/main/java/org/example/SwingChat.java
@@ -6,26 +6,35 @@ import java.awt.event.ActionListener;
 
 public class SwingChat {
     private JPanel mainPanel;
-    private JTextField textFieldNombreUsuario;
+    private static final JFrame frameBase = new JFrame("SwingChat");
     private JButton buttonAcceder;
+    private JPanel primerPanel;
+    private JLabel labelNombre;
+    private JTextField textFieldNombreUsuario;
+    private JPanel segundoPanel;
+    private JButton button1;
 
     public SwingChat() {
         buttonAcceder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                primerPanel.setVisible(false);
+                frameBase.setContentPane(new SwingChat().segundoPanel);
+                segundoPanel.setVisible(true);
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("SwingChat");
-        frame.setContentPane(new SwingChat().mainPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setSize(700, 400);
-        frame.setVisible(true);
+
+        frameBase.setContentPane(new SwingChat().mainPanel);
+        frameBase.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameBase.pack();
+        frameBase.setLocationRelativeTo(null);
+        frameBase.setResizable(false);
+        frameBase.setSize(700, 400);
+        frameBase.setVisible(true);
+
+        frameBase.setContentPane(new SwingChat().primerPanel);
     }
 }
